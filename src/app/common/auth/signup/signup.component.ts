e057d895b4dde036d7e78d4fab6297b24a0ce7a2
JasonRenna <55115087+JasonRenna@users.noreply.github.com>
2019-10-17 13:55:28 -0400

    Same position but using taverns service now
    Same position but using taverns service now

--- src/app/common/auth/signup/signup.component.ts
+++ src/app/common/auth/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { isNull } from '@angular/compiler/src/output/output_ast';
-import { taverns } from './taverns.component';
+import { TavernService } from './taverns.service';
 
 @Component({
     templateUrl: './signup.component.html',
@@ -14,9 +14,20 @@ export class SignUpComponent {
     RoleID = null;
     TavernId = null;
     TavernName = '';
-    taverns = taverns;
+    taverns = [];
 
-    constructor(private router: Router, private authService: AuthService) {}
+    constructor(private router: Router, private authService: AuthService, private tavernService: TavernService) 
+    {
+        this.tavernService.getTaverns().subscribe(
+            (response) => {
+              console.log(response);
+              this.taverns = response;
+            },
+            (error) => {
+              console.log(error);
+            }
+          )
+    }
 
     signup(): void {
         const user = { UserName: this.userName, Password: this.password, RoleID: this.RoleID ,Tavern: { ID: this.TavernId, TavernName: this.TavernName}};
